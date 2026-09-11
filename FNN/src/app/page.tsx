097e8304ef36fdc29e0aa7dda1f0e29b453d6e2f
AJtheManager <ayomidejoe48@gmail.com>
2026-09-11 10:32:43 +0100

    enhance home page security section

--- FNN/src/app/page.tsx
+++ FNN/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Features } from "@/components/home/features";
 import { Hero } from "@/components/home/hero";
 import { HowItWorks } from "@/components/home/how-it-works";
+import { Security } from "@/components/home/security";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Features />
       <HowItWorks />
+      <Security />
     </>
   );
 }
